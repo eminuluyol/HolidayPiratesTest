0958cf269670551b66b68499e37d1eed31f5ccd2
Mehmet Emin Uluyol <eminuluyol@Mehmets-MacBook-Pro.local>
2017-07-16 12:04:43 +0300

    Add Base Fragment

---
+++ app/src/main/java/com/taurus/holidaypiratestest/core/BaseFragment.java
@@ -0,0 +1,104 @@
+package com.taurus.holidaypiratestest.core;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import butterknife.ButterKnife;
+import com.hannesdorfmann.fragmentargs.FragmentArgs;
+import com.hannesdorfmann.mosby.mvp.MvpFragment;
+import com.taurus.holidaypiratestest.R;
+
+/**
+ * Created by eminuluyol on 09/07/2017.
+ */
+
+public abstract class BaseFragment<V extends BaseView, P extends BasePresenter<V>> extends
+    MvpFragment<V, P>
+    implements BaseView {
+
+  private static final String TAG_PROGRESS_DIALOG = "PROGRESS_DIALOG";
+
+  private ProgressDialogFragment progressDialogFragment;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    FragmentArgs.inject(this);
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    final View rootView = inflater.inflate(getLayoutResId(), container, false);
+
+    ButterKnife.bind(this, rootView);
+
+    progressDialogFragment = new ProgressDialogFragmentBuilder().build();
+
+    // Fragment locked in portrait screen orientation
+    getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+    return rootView;
+  }
+
+  protected abstract int getLayoutResId();
+
+  public String getDefaultFragmentTag() {
+    return this.getClass().getSimpleName();
+  }
+
+  @Override
+  public void showError(String message) {
+
+    if (TextUtils.isEmpty(message)) return;
+
+    Snackbar snackbar = Snackbar.make(((BaseActivity) getActivity()).getRootView(), message, Snackbar.LENGTH_LONG);
+    snackbar.getView().setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.red));
+    snackbar.show();
+
+  }
+
+  @Override
+  public void showProgress() {
+    if (progressDialogFragment == null || progressDialogFragment.isVisible()) return;
+
+    progressDialogFragment.show(getChildFragmentManager(), TAG_PROGRESS_DIALOG);
+  }
+
+  @Override
+  public void showProgress(String message) {
+    if (progressDialogFragment == null || progressDialogFragment.isVisible()) return;
+
+    progressDialogFragment.setMessage(message);
+    progressDialogFragment.show(getChildFragmentManager(), TAG_PROGRESS_DIALOG);
+  }
+
+  @Override
+  public void setProgressMessage(String message) {
+    progressDialogFragment.setMessage(message);
+  }
+
+  @Override
+  public void dismissProgress() {
+    if(progressDialogFragment == null) return;
+    progressDialogFragment.dismissAllowingStateLoss();
+  }
+
+  @Override
+  public void onStop() {
+    stopApiCall();
+    super.onStop();
+  }
+
+  public void stopApiCall() {
+    getPresenter().clearCompositeDisposable();
+  }
+
+}
